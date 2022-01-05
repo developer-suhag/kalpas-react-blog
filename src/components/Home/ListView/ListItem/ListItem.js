b0715c4482ecee914dbd9d1b11aad9c7e6d31854
Suhag Al Amin <developersuhag@gmail.com>
2022-01-05 23:31:39 +0600

    remove from list item

--- src/components/Home/ListView/ListItem/ListItem.js
+++ src/components/Home/ListView/ListItem/ListItem.js
@@ -2,9 +2,17 @@ import React from "react";
 import "../ListView.css";
 import demoImg from "../../../../images/demo-post-img.jpg";
 import { FaTimes } from "react-icons/fa";
+import { Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { removePost } from "../../../../redux/slices/postSlice";
 
 const ListItem = ({ post }) => {
-  const { title, body } = post;
+  const { id, title, body } = post;
+  //   handleRemove
+  const dispatch = useDispatch();
+  const handleRemove = (id) => {
+    dispatch(removePost(id));
+  };
   return (
     <div className="d-flex justify-content-between align-items-center gap-3">
       <div className="list-item-view d-flex align-items-center gap-4 mb-4 p-4 bg-white shadow-sm">
@@ -14,9 +22,13 @@ const ListItem = ({ post }) => {
           <p>{body}</p>
         </div>
       </div>
-      <div className="close-icon">
+      <Button
+        onClick={() => handleRemove(id)}
+        variant=""
+        className="close-icon"
+      >
         <FaTimes />
-      </div>
+      </Button>
     </div>
   );
 };
