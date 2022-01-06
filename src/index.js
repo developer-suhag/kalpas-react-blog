146b06155f2b4dbefb4ab4697568f2b94a0714da
Suhag Al Amin <developersuhag@gmail.com>
2022-01-06 18:55:15 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import persistStore from "redux-persist/es/persistStore";
-import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
 import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
-const persistor = persistStore(store);
-
 ReactDOM.render(
   <React.StrictMode>
     {/* redux provider  */}
     <Provider store={store}>
-      {/* persisgate  */}
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  persistReducer,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import postSlice from "./slices/postSlice";
 
-const persistConfig = {
-  key: "books",
-  storage,
-};
-
-const reducers = combineReducers({ posts: postSlice });
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-
-export default configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+export const store = configureStore({
+  reducer: {
+    posts: postSlice,
+  },
 });
+
+export default store;
