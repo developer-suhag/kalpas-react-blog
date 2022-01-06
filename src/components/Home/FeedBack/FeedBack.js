2a72cacb0abc4163708bd413518830b0372bf0b8
Suhag Al Amin <developersuhag@gmail.com>
2022-01-06 12:10:00 +0600

    feedback show

--- src/components/Home/FeedBack/FeedBack.js
+++ src/components/Home/FeedBack/FeedBack.js
@@ -1,25 +1,58 @@
-import React, { useState } from "react";
-import { Button, Offcanvas } from "react-bootstrap";
+import React from "react";
+import { Button, Col, Offcanvas, Row } from "react-bootstrap";
+import user from "../../../images/user.jpg";
+import "./FeedBack.css";
 
 const FeedBack = ({ showFeedBack, handleFeedBackClose }) => {
-  //   const [showFeedBack, setShowFeedBack] = useState(false);
+  // feedback
 
-  //   const handleFeedBackClose = () => setShowFeedBack(false);
-  //   const handleFeedBackShow = () => setShowFeedBack(true);
   return (
     <>
-      {/* <Button variant="primary" onClick={handleFeedBackShow}>
-        Launch
-      </Button> */}
+      <Offcanvas
+        className="w-75 feedback"
+        show={showFeedBack}
+        onHide={handleFeedBackClose}
+      >
+        <Row>
+          <Col md={4}>
+            <div className="side-bar py-5 ps-2">
+              {/* user  */}
+              <div className="d-flex align-items-center gap-3 bg-white p-2 rounded-3 shadow">
+                <div>
+                  <img className="user" src={user} alt="" />
+                </div>
+                <div>
+                  <h4>Hi, Reader</h4>
+                  <p>Here's your News!</p>
+                </div>
+              </div>
 
-      <Offcanvas show={showFeedBack} onHide={handleFeedBackClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-        </Offcanvas.Body>
+              {/* feedback  */}
+              <div className="bg-white p-3 mt-4 rounded-3 shadow text-center">
+                <h3>Have a Feedback?</h3>
+                <Button
+                  onClick={handleFeedBackClose}
+                  className="kalpas-btn-feedback"
+                  variant=""
+                >
+                  We're lisentening!
+                </Button>
+              </div>
+            </div>
+          </Col>
+          <Col md={8}>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title className="">
+                <h3> Thank you so much for taking the time!</h3>
+                <p>Please provide the below details</p>
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              Some text as placeholder. In real life you can have the elements
+              you have chosen. Like, text, images, lists, etc.
+            </Offcanvas.Body>
+          </Col>
+        </Row>
       </Offcanvas>
     </>
   );
