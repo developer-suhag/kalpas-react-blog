6d4a617b3b63206dda77517518da9842eeae76e8
Suhag Al Amin <developersuhag@gmail.com>
2022-01-07 16:18:56 +0600

    initial pagination

--- src/components/Home/ListView/ListView.js
+++ src/components/Home/ListView/ListView.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../../redux/slices/postSlice";
 import ListItem from "./ListItem/ListItem";
@@ -12,11 +13,53 @@ const ListView = () => {
   }, [dispatch]);
   // get posts from redux
   const { allPosts } = useSelector((state) => state.posts);
+
+  // pagination
+  const [offset, setOffset] = useState(0);
+  const [post, setPost] = useState([]);
+  const [perPage] = useState(6);
+  const [pageCount, setPageCount] = useState(0);
+
+  const getData = async () => {
+    const posts = allPosts;
+    const slice = posts.slice(offset, offset + perPage);
+    const postData = slice.map((post) => (
+      <div key={post.id}>
+        <ListItem key={post.id} post={post} />
+      </div>
+    ));
+    setPost(postData);
+    setPageCount(Math.ceil(posts.length / perPage));
+  };
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [offset]);
+
   return (
     <div>
-      {allPosts.slice(0, 6).map((post) => (
+      {/* {allPosts.slice(0, 6).map((post) => (
         <ListItem key={post.id} post={post} />
-      ))}
+      ))} */}
+      {post}
+      <ReactPaginate
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+      />
     </div>
   );
 };
